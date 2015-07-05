de78e182322682e8e82bba098b992340ec822453
ievgen <yevhen.lychko@gmail.com>
2015-07-05 16:16:09 -0400

    Committtted 2

--- src/main/java/currencyparser/App.java
+++ src/main/java/currencyparser/App.java
@@ -26,9 +26,7 @@ public class App {
         }
 
         System.out.println("Changes!");
-        System.out.println("Changes4!");
-        System.out.println("Changes3!");
-        System.out.println("Changes2!");
+
 
     }
 }
